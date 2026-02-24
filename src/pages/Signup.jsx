ea27298a53ed0dd12fcb4a9624f7a4f53d951dc0
MohamedShalikM <mohamedshalik25.it@gmail.com>
2026-02-24 11:45:40 +0530

    signup error fixed

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -56,7 +56,7 @@ export default function Signup() {
 
     setIsLoading(true);
     try {
-       const res = await fetch(`${API_URL}/api/auth/register`, {
+       const res = await fetch(`${API_URL}/api/auth/login`, {
          method: "POST",
          headers: { "Content-Type": "application/json" },
          body: JSON.stringify(formData),
